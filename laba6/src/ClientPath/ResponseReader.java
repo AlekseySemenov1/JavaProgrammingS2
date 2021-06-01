a90ca28e96f7c4131f59c1768cc17343a996da3b
AlekseySemenov1 <71176575+AlekseySemenov1@users.noreply.github.com>
2021-06-01 12:16:26 +0500

    Update ResponseReader.java

--- laba6/src/ClientPath/ResponseReader.java
+++ laba6/src/ClientPath/ResponseReader.java
@@ -15,19 +15,32 @@ public class ResponseReader {
         this.socket = socket;
     }
 
-    public void readResponse() {
+    public boolean readResponse() {
         try {
             byte[] message = new byte[100000];
             DatagramPacket packet1 = new DatagramPacket(message, message.length);
+            socket.setSoTimeout(1000);
             socket.receive(packet1);
+
             ByteArrayInputStream bis = new ByteArrayInputStream(message);
             ObjectInputStream ois = new ObjectInputStream(bis);
             Response response = (Response) ois.readObject();
+
+            if (response.getMessage().equals("Нет элемента с таким id")){
+                System.out.println(response.getMessage());
+                return false;
+            }
             System.out.println(response.getMessage());
+            return true;
+        } catch (SocketTimeoutException e){
+            System.out.println("Превышено время ожидани ответа от сервера/сервер недоступен");
+        } catch (EOFException e){
+            System.out.println("Слишком большой объем данных");
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        return false;
     }
 }
