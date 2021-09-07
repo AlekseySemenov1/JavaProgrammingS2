d817a61d3969a5d745d67ab928bb0936268fd4f1
AlekseySemenov1 <71176575+AlekseySemenov1@users.noreply.github.com>
2021-09-07 19:50:49 +0500

    Update CommandReader.java

--- laba7/ClientPath/src/ClientPath/CommandReader.java
+++ laba7/ClientPath/src/ClientPath/CommandReader.java
@@ -4,7 +4,7 @@ import CollectionElements.Discipline;
 import CollectionElements.LabWork;
 import CollectionElements.User;
 import Command.*;
-import Messages.Request;
+import Messages.CommandRequest;
 import Other.ParametrsInput;
 import exceptions.EmptyInputException;
 import exceptions.RecursionException;
@@ -88,82 +88,88 @@ public class CommandReader {
     }
 
     public void executeCommand(String[] args) {
-        switch (args[0]) {
-            case "add":
-                LabWork laba = ((ComAdd) commandsList.get(0)).askArgs(pI, user);
-                writer.writeCommandRequest(new Request(commandsList.get(0), laba, user));
-                break;
+        try{
+            switch (args[0]) {
+                case "add":
+                    LabWork laba = ((ComAdd) commandsList.get(0)).askArgs(pI, user);
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(0), laba, user));
+                    break;
 
-            case "clear":
-                writer.writeCommandRequest(new Request(commandsList.get(1), user));
-                break;
+                case "clear":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(1), user));
+                    break;
 
-            case "count_less_than_discipline":
-                Discipline discipline = ((ComCountLessDiscipline) commandsList.get(2)).askArgs(pI);
-                writer.writeCommandRequest(new Request(commandsList.get(2), discipline, user));
-                break;
+                case "count_less_than_discipline":
+                    Discipline discipline = ((ComCountLessDiscipline) commandsList.get(2)).askArgs(pI);
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(2), discipline, user));
+                    break;
 
-            case "print_field_descending_difficulty":
-                writer.writeCommandRequest(new Request(commandsList.get(5), user));
-                break;
+                case "print_field_descending_difficulty":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(5), user));
+                    break;
 
-            case "filter_by_difficulty":
-                writer.writeCommandRequest(new Request(commandsList.get(3), args[1], user));
-                break;
+                case "filter_by_difficulty":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(3), args[1], user));
+                    break;
 
-            case "help":
-                writer.writeCommandRequest(new Request(commandsList.get(6), user));
-                break;
+                case "help":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(6), user));
+                    break;
 
-            case "history":
-                writer.writeCommandRequest(new Request(commandsList.get(7), user));
-                break;
+                case "history":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(7), user));
+                    break;
 
-            case "info":
-                writer.writeCommandRequest(new Request(commandsList.get(8), user));
-                break;
+                case "info":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(8), user));
+                    break;
 
-            case "remove_by_id":
-                writer.writeCommandRequest(new Request(commandsList.get(9), args[1], user));
-                break;
+                case "remove_by_id":
+                    if (args[1].equals("")) throw new EmptyInputException();
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(9), args[1], user));
+                    break;
 
-            case "remove_head":
-                writer.writeCommandRequest(new Request(commandsList.get(10), user));
-                break;
+                case "remove_head":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(10), user));
+                    break;
 
-            case "remove_lower":
-                System.out.println("Введите элемент для сравнения, сравнение производится по id");
-                LabWork laba1 = ((ComRemoveLower) commandsList.get(11)).askArgs(pI, user);
-                writer.writeCommandRequest(new Request(commandsList.get(11), laba1, user));
-                break;
+                case "remove_lower":
+                    System.out.println("Введите элемент для сравнения, сравнение производится по id");
+                    LabWork laba1 = ((ComRemoveLower) commandsList.get(11)).askArgs(pI, user);
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(11), laba1, user));
+                    break;
 
-            case "execute_script":
-                writer.writeCommandRequest(new Request(commandsList.get(12), user));
-                scriptinput(args[1]);
-                break;
+                case "execute_script":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(12), user));
+                    scriptinput(args[1]);
+                    break;
 
-            case "show":
-                writer.writeCommandRequest(new Request(commandsList.get(13), user));
-                break;
+                case "show":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(13), user));
+                    break;
 
-            case "update_by_id":
-                if (writer.writeCommandRequest(new Request(commandsList.get(14), args[1], user)) == 1) {
-                    LabWork laba2 = ((ComUpdateById) commandsList.get(14)).askArgs(args[1], pI, user);
-                    laba2.setId(Long.parseLong(args[1]));
-                    writer.writeCommandRequest(new Request(commandsList.get(14), laba2, user));
-                }
-                break;
+                case "update_by_id":
+                    if (args[1].equals("")) throw new EmptyInputException();
+                    if (writer.writeCommandRequest(new CommandRequest(commandsList.get(14), args[1], user)) == 1) {
+                        LabWork laba2 = ((ComUpdateById) commandsList.get(14)).askArgs(args[1], pI, user);
+                        laba2.setId(Long.parseLong(args[1]));
+                        writer.writeCommandRequest(new CommandRequest(commandsList.get(14), laba2, user));
+                    }
+                    break;
 
-            case "exit":
-                writer.writeCommandRequest(new Request(commandsList.get(4), user));
-                System.exit(0);
+                case "exit":
+                    writer.writeCommandRequest(new CommandRequest(commandsList.get(4), user));
+                    System.exit(0);
 
-            case "":
-                System.out.println("Введена пустая строка");
-                break;
+                case "":
+                    System.out.println("Введена пустая строка");
+                    break;
 
-            default:
-                System.out.println("Нет такой команды, введите команду help, чтобы просмотреть список доступных команд");
+                default:
+                    System.out.println("Нет такой команды, введите команду help, чтобы просмотреть список доступных команд");
+            }
+        } catch (EmptyInputException e) {
+            System.out.println("Недопустимое значение аргумента");
         }
     }
 
